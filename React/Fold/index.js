871de45985799e988fa72bd08130ac593fc0ccd8
zer0 <u9648u6653u52c7@gmail.com>
2021-03-26 12:31:06 +0800

    refactor(Fold):make it much flexible

--- React/Fold/index.js
+++ React/Fold/index.js
@@ -1,33 +1,53 @@
-import "./style/index.scss";
-import React, { useState } from "react";
+import { isArray, isFunction } from "./type";
+import React, { useState, useMemo } from "react";
 
-export default function (props) {
+export default function Fold(props) {
     const {
-        children,
-        className,
-        style,
-        pageSize = 3,
-        text = "查看更多"
+        triggerElem,
+        rtriggerElem,
+        initialNum = 3,
+        wrapElem,
+        children
     } = props;
-    const [num, setNum] = useState(pageSize);
-    const size = React.Children.count(children);
+
     const arr = React.Children.toArray(children);
+    const maxNum = arr.length;
+    const [num, setNum] = useState(initialNum);
+    // 点击事件
+    const getNewElem = (el, n) => (React.isValidElement(el)
+        ? React.cloneElement(el, {
+            onClick: evt => {
+                const { props: { onClick } } = el;
+                isFunction(onClick) && onClick(evt);
+                setNum(n);
+            }
+        }) : null);
+    // 正向触发组件
+    const tElem = useMemo(
+        () => getNewElem(triggerElem, maxNum),
+        [triggerElem]
+    );
+    // 反向触发组件
+    const rtElem = useMemo(
+        () => getNewElem(rtriggerElem, initialNum),
+        [rtriggerElem]
+    );
 
-    let clsName = "view-more";
-    if (typeof className === "string") {
-        clsName += " " + className;
-    }
+    const elems = (() => {
+        if (maxNum > 0 && initialNum < maxNum) {
+            let el = arr.slice(0, num);
+            el = React.isValidElement(wrapElem)
+                ? React.cloneElement(wrapElem, { children: el })
+                : el;
+            return <React.Fragment>
+                {el} {maxNum > num ? tElem : rtElem}
+            </React.Fragment>;
+        } else {
+            return React.isValidElement(wrapElem)
+                ? React.cloneElement(wrapElem, { children })
+                : children;
+        }
+    })();
 
-    const elem = <div className={clsName}
-        style={{ ...style }}
-        onClick={() => setNum(size)}>
-        <span className="va-m">
-            <em className="va-m">{text}</em>
-        </span>
-    </div>;
-
-    return (size > 0 ? <>
-        {arr.slice(0, num)}
-        {size > num ? elem : null}
-    </> : null);
+    return elems;
 }
